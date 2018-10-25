16f2f8084fe388d137d272046191c05e57d7e987
Christoph <christoph.sax@gmail.com>
2018-10-25 22:03:29 +0200

    no tsbox requried for ts and mts

--- R/BDFM.R
+++ R/BDFM.R
@@ -1,4 +1,4 @@
-BDFM <- function(Y, m, p, FC, Bp = NULL, lam_B = 0, Hp = NULL, lam_H = 0, nu_q = 0, nu_r = NULL, ID = "PC_full", ITC = T, reps = 1000, burn = 500, Loud = FALSE) {
+BDFM <- function(Y, m, p, FC, Bp = NULL, lam_B = 0, Hp = NULL, lam_H = 0, nu_q = 0, nu_r = NULL, ID = "PC_full", ITC = T, reps = 1000, burn = 500, loud = FALSE) {
 
   # ----------- Preliminaries -----------------
   Y <- as.matrix(Y)
@@ -74,7 +74,7 @@ BDFM <- function(Y, m, p, FC, Bp = NULL, lam_B = 0, Hp = NULL, lam_H = 0, nu_q =
     r   <- r + FC
   }
 
-  Parms <- EstDFM(B = B_in, Bp = Bp, lam_B = lam_B, q = q, nu_q = nu_q, H = H, Hp = Hp, lam_H = lam_H, R = Rvec, nu_r = nu_r, Y = Y, reps = reps, burn = burn, Loud = Loud)
+  Parms <- EstDFM(B = B_in, Bp = Bp, lam_B = lam_B, q = q, nu_q = nu_q, H = H, Hp = Hp, lam_H = lam_H, R = Rvec, nu_r = nu_r, Y = Y, reps = reps, burn = burn, Loud = loud)
 
   if (ID %in% c("PC_sub", "PC_full")) {
     B <- Parms$B
@@ -135,15 +135,15 @@ BDFM <- function(Y, m, p, FC, Bp = NULL, lam_B = 0, Hp = NULL, lam_H = 0, nu_q =
 
 
 #' MCMC Routine for Bayesian Dynamic Factor Models
-#' 
+#'
 #' \code{CppBDFM} is the core C++ function for estimating a linear-Gaussian Bayesain dynamic factor model by MCMC methods using Durbin and Koopman's disturbance smoother. This function may be called directly by advanced users. The only dependencies are the  Armadillo (\url{http://arma.sourceforge.net/}) linear algebra library for C++ and the packages needed for interfacing with R (\code{\link{Rcpp}} and \code{\link{RcppArmadillo}}).
-#' 
-#' @param B  initial guess for B in transition equation 
+#'
+#' @param B  initial guess for B in transition equation
 #' @param Bp prior for B
 #' @param lam_B prior tightness for B (additive)
 #' @param q initial guess for q in the transition equation
 #' @param nu_q prior "degrees of freedom" for inverse-Whishart prior for q (additive, prior scale is fixed so that increasing nu_q shrinks the variance towards zero)
-#' @param H initial guess for H in the trasition equation 
+#' @param H initial guess for H in the trasition equation
 #' @param Hp prior for H
 #' @param lam_H prior tightness for H (additive)
 #' @param R initial guess for diagonal elements of R in the transition equation, entered as a vector
