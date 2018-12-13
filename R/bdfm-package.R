748ffa681185d368b4d826412fc41593c65ba811
Christoph <christoph.sax@gmail.com>
2018-12-13 14:01:28 +0700

    documentation and namespace

---
+++ R/bdfm-package.R
@@ -0,0 +1,24 @@
+#' Dates of Chinese New Year, Indian Diwali and Easter
+#'
+#' Dates of Chinese New Year, Indian Diwali and Easter, suitable for the use in [seas_we()].
+#'
+#' @docType data
+#'
+#' @format Objects of class `"Date"`.
+#'
+#' @source
+#' \url{http://www.census.gov/srd/www/genhol/genhol_downloads.html}
+#'
+#' \url{http://www.chinesenewyears.info/chinese-new-year-calendar.php}
+#'
+#' Ministry of Statistics and Programme Implementation
+#'
+#' @name easter
+#' @aliases cny diwali
+#' @keywords datasets
+#' @examples
+#'
+#' cny
+#' diwali
+#' easter
+NULL
