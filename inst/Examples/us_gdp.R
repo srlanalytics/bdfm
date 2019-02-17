6904a01b2dbb91186a981dcc67e5a1c646c4a66f
Christoph <christoph.sax@gmail.com>
2019-02-17 15:04:21 +0100

    reprex error, as in issue

--- inst/Examples/us_gdp.R
+++ inst/Examples/us_gdp.R
@@ -1,3 +1,4 @@
+library(bdfm)
 
 # drop outliers (optional but gets rid of some wierd stuff)
 econ_us[abs(scale(econ_us)) > 4] <- NA
@@ -22,15 +23,8 @@ diffs <- setdiff(colnames(econ_us), c("A191RL1Q225SBEA", 'W068RCQ027SBEA', "USSL
 
 m <- dfm(econ_us, factors = 3, pre_differenced = "A191RL1Q225SBEA", logs = logs, diffs = diffs)
 
-# warning: eig_gen(): decomposition failed
-
-# error: max(): object has no elements
 # Error in EstDFM(B = B_in, Bp = Bp, Jb = Jb, lam_B = lam_B, q = q, nu_q = nu_q,  :
-#   max(): object has no elements
-# In addition: Warning message:
-# In bdfm(Y = Y, m = m, p = p, Bp = Bp, lam_B = lam_B, Hp = Hp, lam_H = lam_H,  :
-#   PC_full not a valid identification string or index vector, defaulting to pc_full
-# >
+#   c++ exception (unknown reason)
 
 # - [ ] What is wrong?
 # - [ ] Better Error that tells me whats wrong
@@ -39,6 +33,6 @@ m <- dfm(econ_us, factors = 3, pre_differenced = "A191RL1Q225SBEA", logs = logs,
 # ts.plot(m$Qstore[1,1,])
 # ts.plot(m$Hstore[1,1,])
 
-summary(m)
+# summary(m)
 
 
