548bc9bac762551807b0e0a004361514586ec501
SRLanalytics <seth@srlanalytics.com>
2019-03-05 17:52:10 -1000

    Forecast updates by variable

--- R/dfm_core.R
+++ R/dfm_core.R
@@ -1,26 +1,27 @@
-m <- 1
-p <- "auto"
-freq <- "auto"
-Bp <- NULL
-preD <- 1
-lam_B = 0
-trans_df = 0
-Hp = NULL
-lam_H = 0
-obs_df = NULL
-ID = "pc_long"
-store_idx = 1
-reps = 1000
-burn = 500
-verbose = T
-tol = .01
-FC = 3
-logs = NULL
-diffs = NULL
+# m <- 1
+# p <- "auto"
+# freq <- "auto"
+# method = "bayesian"
+# Bp <- NULL
+# preD <- 1
+# lam_B = 0
+# trans_df = 0
+# Hp = NULL
+# lam_H = 0
+# obs_df = NULL
+# ID = "pc_long"
+# store_idx = 1
+# reps = 1000
+# burn = 500
+# verbose = T
+# tol = .01
+# FC = 3
+# # logs = NULL
+# # diffs = NULL
 # logs = c( 2,  4,  5,  8,  9, 10, 11, 12, 15, 16, 17, 21, 22)
 # diffs = c(2, 4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)
-outlier_threshold <- 4
-scale = TRUE
+# outlier_threshold <- 4
+# scale = TRUE
 
 dfm_core <- function(Y, m, p, FC, method, scale, logs, outlier_threshold, diffs, freq, preD,
                      Bp, lam_B, trans_df, Hp, lam_H, obs_df, ID,
@@ -124,6 +125,14 @@ dfm_core <- function(Y, m, p, FC, method, scale, logs, outlier_threshold, diffs,
       ID = ID, reps = reps, burn = burn
     )
   }
+  
+  
+  #Get forecast updates at each period
+  factor_update <- lapply(X = seq(to = NROW(Y)), FUN = factor_updates, gain = lapply(est$Kstore, one_through_m, m = m), PE = est$PEstore)
+  factor_update <- lapply(X = seq(to = NROW(Y)), FUN = name_updates, Y = Y, fc_update = factor_update) #name updates
+  est$Kstore  <- NULL #this is huge and no longer needed, so drop it
+  est$PEstore <- NULL #this is huge and no longer needed, so drop it
+  est$factor_update <- factor_update #return this instead --- far more useful!
 
   # undo scaling
   if(scale){
@@ -131,6 +140,15 @@ dfm_core <- function(Y, m, p, FC, method, scale, logs, outlier_threshold, diffs,
     if(!is.null(store_idx) && method == "bayesian"){
       est$Ystore <- est$Ystore*(y_scale[store_idx]/100) + y_center[store_idx]
       est$Ymedain <- est$Ymedian*(y_scale[store_idx]/100) + y_center[store_idx]
+    }
+  }
+  
+  # get updates to store_idx if specified
+  if(!is.null(store_idx)){
+    if(scale){
+      est$idx_update <- lapply(factor_update, FUN = multiply_each, loading = t(est$H[store_idx,,drop=FALSE]), y_scale = y_scale[store_idx]/100)
+    }else{
+      est$idx_update <- lapply(factor_update, FUN = multiply_each, loading = t(est$H[store_idx,,drop=FALSE]), y_scale = 1)
     }
   }
 
