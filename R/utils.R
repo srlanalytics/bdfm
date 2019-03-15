1e351ab456f4d53a8e3986c94765f22c8ec2a6a7
SRLanalytics <seth@srlanalytics.com>
2019-03-15 12:51:10 -1000

    drop unused function Y_sub

--- R/utils.R
+++ R/utils.R
@@ -6,30 +6,6 @@ NumNA <- function(y) {
   length(which(is.na(y)))
 }
 
-Y_sub <- function(Y) {
-  # lots of ways to select a full submatrix of the data... this is one idea
-  Ysub <- na.omit(Y)
-  obs_old <- nrow(Ysub) * ncol(Ysub)
-  miss_rows <- apply(Y, MARGIN = 2, FUN = NumNA)
-  dims <- dim(na.omit(Y[, -which(miss_rows == max(miss_rows))]))
-  obs_new <- dims[1] * dims[2]
-  if (obs_new < obs_old) {
-    ind <- apply(Y, MARGIN = 1, FUN = AnyNA)
-    Ysub <- Y[!ind, ]
-  }
-  while (obs_new > obs_old || obs_old == 0) {
-    obs_old <- obs_new
-    miss_rows <- apply(Y, MARGIN = 2, FUN = NumNA)
-    Y <- Y[, -which(miss_rows == max(miss_rows))]
-    ind <- apply(Y, MARGIN = 1, FUN = AnyNA) # indexes of Y that do not correspond to Ysub
-    Ysub <- Y[!ind, ]
-    # Ysub      <- na.omit(Y)
-    obs_new <- nrow(Ysub) * ncol(Ysub)
-    # print(dim(Ysub))
-  }
-  return(list(Ysub = Ysub, ind = !ind))
-}
-
 # convert string to numeric index value
 standardize_index <- function(x, Y) {
   argname <- deparse(substitute(x))
