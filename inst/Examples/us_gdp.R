b24d31321508d17686d10de1170c3c9b2abad5cf
Christoph <christoph.sax@gmail.com>
2019-03-24 14:48:53 +0100

    back to non auto model (for now)

--- inst/Examples/us_gdp.R
+++ inst/Examples/us_gdp.R
@@ -4,27 +4,27 @@ library(bdfm)
 # To see which variables will be differenced or log differenced use:
 # log_diffs <- should_log_diff(econ_us)
 # Altermatively, specify logs and diffs manually:
-# logs <- c(
-#   "W068RCQ027SBEA",
-#   "PCEDG",
-#   "PCEND",
-#   "JTSJOL",
-#   "INDPRO",
-#   "CSUSHPINSA",
-#   "HSN1F",
-#   "TSIFRGHT",
-#   "IPG2211S",
-#   "DGORDER",
-#   "AMTMNO",
-#   "CPILFESL",
-#   "ICSA"
-# )
-#
-# diffs <- setdiff(colnames(econ_us), c("A191RL1Q225SBEA", "USSLIND"))
+logs <- c(
+  "W068RCQ027SBEA",
+  "PCEDG",
+  "PCEND",
+  "JTSJOL",
+  "INDPRO",
+  "CSUSHPINSA",
+  "HSN1F",
+  "TSIFRGHT",
+  "IPG2211S",
+  "DGORDER",
+  "AMTMNO",
+  "CPILFESL",
+  "ICSA"
+)
+
+diffs <- setdiff(colnames(econ_us), c("A191RL1Q225SBEA", "USSLIND"))
 
 # Forecasts should ALWAYS be made using store_idx if we are interested in forcasting
 # one of the series in the model.
-m <- dfm(data = econ_us, factors = 3, pre_differenced = "A191RL1Q225SBEA", store_idx = "A191RL1Q225SBEA")
+m <- dfm(data = econ_us, logs = logs, diffs = diffs, factors = 3, pre_differenced = "A191RL1Q225SBEA", store_idx = "A191RL1Q225SBEA")
 
 # Are we drawing from a stationary distribution?
 ts.plot(m$Qstore[1,1,])
